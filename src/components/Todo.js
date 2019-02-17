0e3934844a908c18e68f2b64fbc9fce1925f5028
Ryan Walker <rytwalker@gmail.com>
2019-02-17 02:01:26 -0500

    basic todo

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Todo = props => {
+  const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const inputChangeHandler = event => {
+    setTodoName(event.target.value);
+  };
+
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName));
+  };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={todoName}
+      />
+      <button onClick={todoAddHandler} type="button">
+        Add
+      </button>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default Todo;
